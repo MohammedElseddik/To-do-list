bb7f87658a416dcf007281fa678818f5f77c471e
MohammedElseddik <rebbouh.mohammed.elseddik@gmail.com>
2022-02-20 14:18:05 +0100

    Add the deletion functionality to the app

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const addForm = document.querySelector('.add__task');
 const list = document.querySelector('.tasks__body');
+const todoTask = document.getElementById('add-task');
+
+// Add Tasks section
 
 const addTask = (task) => {
     const todiListItems = document.createElement('li');
-    todiListItems.innerHTML =`
+    todiListItems.innerHTML = `
     <span>${task}</span>
-    <i class="fa-solid fa-trash-can delete"></i>`;
+    <i class="fa-solid fa-trash-can trash-icon"></i>`;
     list.appendChild(todiListItems);
 }
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const todoTask = document.getElementById('add-task').value.trim();
-    if (todoTask.length) {
-        addTask(todoTask);
+    if (todoTask.value.trim().length) {
+        addTask(todoTask.value.trim());
         addForm.reset();
+    }
+});
+
+//Delete Tasks section
+
+list.addEventListener('click', (e) => {
+    console.log(e);
+    if (e.target.classList.contains('trash-icon')) {
+        e.target.parentElement.remove();
     }
 });
